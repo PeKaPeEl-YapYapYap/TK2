bc5c2dcea4ce8d0b7c1fed487adf2d2ecfe571e1
Narwhll <davin.fauzaan@gmail.com>
2026-04-01 22:58:41 +0700

    made the initial page without login yet

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+# class CustomUser(AbstractUser):
+#     # npm, prodi, phtoroute
+#     prodiChoices = ['Ilmu Komputer', 'Sistem Informasi', 'Kecerdasaan Artifisial']
+
+#     npm = models.CharField(max_length=10)
+#     prodi = models.CharField(choices=prodiChoices, default='Ilmu Komputer')
+#     photoRoute = models.TextField()
+    
+
+class ConfigWeb(models.Model):
+    background_color = models.CharField(max_length=50, default="#ffffff", help_text="Contoh: #f0f0f0 atau lightblue")
+    font_family = models.CharField(max_length=100, default="Arial, sans-serif", help_text="Contoh: 'Courier New', Courier, monospace")
+
+    def __str__(self):
+        return "Pengaturan Tampilan Website"
--- main/views.py
+++ main/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    data_biodata = [
+        {
+            "nama": "Davin Fauzan", 
+            "umur": 19, 
+            "npm" : "2406409504",
+            "prodi": "Ilmu Komputer", 
+            "gambar": "/static/image/davin.png"
+        },
+        {
+            "nama": "Peter Yap", 
+            "umur": 19, 
+            "npm": "2406432910",
+            "prodi": "Ilmu Komputer", 
+            "gambar": "/static/image/peter.jpg"
+        },
+        {
+            "nama": "Farrell Bagoes Rahmantyo", 
+            "umur": 21, 
+            "npm": "2406420596", 
+            "prodi": "Ilmu Komputer",
+            "gambar": "/static/image/farrell.jpg"
+        },
+        {
+            "nama": "Andrew Wanarahardja", 
+            "umur": 19, 
+            "npm": "2406407373",
+            "prodi": "Ilmu Komputer", 
+            "gambar": "/static/image/andrew.png"
+        },
+		{
+			"nama": "Rousan Chandra Syahbunan", 
+			"umur": 69, 
+			"npm": "2406435894",
+			"prodi": "Ilmu Komputer", 
+			"gambar": "/static/image/rousan.jpg"
+		},
+    ]
+    return render(request, "main/index.html", {"biodata_list": data_biodata})
